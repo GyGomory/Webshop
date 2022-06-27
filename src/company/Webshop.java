818b1c302f65aef035b22842c3a7eec1f523a382
GyGomory <gyulagomory@gmail.com>
2022-06-27 16:16:21 +0200

    Elsó Push

--- src/company/Webshop.java
+++ src/company/Webshop.java
@@ -1,4 +1,11 @@
 package company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Webshop {
+
+
+    List<User> user = new ArrayList<>();
+    List<Product> products =  new ArrayList<>();
 }
